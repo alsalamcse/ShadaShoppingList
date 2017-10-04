7cf5a2f7319c4b75df2c65a7da821f804568484a
alsalamcc <alsalamcc17>
2017-10-04 13:10:34 +0300

    jkuij

--- app/src/main/java/com/hsarme/shada/shadashoppinglist/LogIn.java
+++ app/src/main/java/com/hsarme/shada/shadashoppinglist/LogIn.java
@@ -7,13 +7,18 @@ import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity
 {
-    private EditText email, password;
-    private Button signIn, signUp ,
+    private EditText etEmail, etPassword;
+    private Button btnIn, btnUp, btnForget;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        etEmail=(EditText)findViewById(R.id.etEmail);
+        etPassword=(EditText)findViewById(R.id.etPassword);
+        btnIn=(Button) findViewById(R.id.btnIn);
+        btnUp=(Button)findViewById(R.id.btnUp);
+        btnForget=(Button)findViewById(R.id.btnForget);
     }
 }
---
+++ app/src/main/java/com/hsarme/shada/shadashoppinglist/SignUp.java
@@ -0,0 +1,26 @@
+package com.hsarme.shada.shadashoppinglist;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUp extends AppCompatActivity
+{
+    private EditText etName, etEmail2, etPassword2, etRePassword, etPhone;
+    private Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        etName=(EditText)findViewById(R.id.etName);
+        etEmail2=(EditText)findViewById(R.id.etEmail2);
+        etPassword2=(EditText)findViewById(R.id.etPassword;
+        etRePassword=(EditText)findViewById(R.id.etRePassword);
+        etPhone=(EditText)findViewById(R.id.etPhone);
+        btnSave=(Button)findViewById(R.id.btnSave);
+
+    }
+}

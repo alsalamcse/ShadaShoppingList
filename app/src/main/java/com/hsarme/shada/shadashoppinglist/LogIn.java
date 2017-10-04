daf07f8281da25ffee427195e6e301cee039dd15
alsalamcc <alsalamcc17>
2017-10-04 10:51:51 +0300

    jkuij

--- app/src/main/java/com/hsarme/shada/shadashoppinglist/LogIn.java
+++ app/src/main/java/com/hsarme/shada/shadashoppinglist/LogIn.java
@@ -2,8 +2,14 @@ package com.hsarme.shada.shadashoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LogIn extends AppCompatActivity {
+public class LogIn extends AppCompatActivity
+{
+    private EditText email, password;
+    private Button signIn, signUp ,
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

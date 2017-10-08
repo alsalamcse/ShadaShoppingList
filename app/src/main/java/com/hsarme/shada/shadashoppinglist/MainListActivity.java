b81ba7f35c285248e94c98fefa2b83751aca6640
alsalamcc <alsalamcc17>
2017-10-08 13:21:36 +0300

    jkuij

--- app/src/main/java/com/hsarme/shada/shadashoppinglist/MainListActivity.java
+++ app/src/main/java/com/hsarme/shada/shadashoppinglist/MainListActivity.java
@@ -19,6 +19,9 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import com.hsarme.shada.shadashoppinglist.mainListFragments.CurrentListFragment;
+import com.hsarme.shada.shadashoppinglist.mainListFragments.HistoryFragment;
+
 public class MainListActivity extends AppCompatActivity {
 
     /**
@@ -30,6 +33,8 @@ public class MainListActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
+    private CurrentListFragment currentListFragment;
+    private HistoryFragment historyFragment;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -129,6 +134,7 @@ public class MainListActivity extends AppCompatActivity {
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
+
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
         }
@@ -137,24 +143,34 @@ public class MainListActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
+            if (position==0)
+            {
+                if (currentListFragment==null)
+                    currentListFragment =new CurrentListFragment();
+                return currentListFragment;
+            }
+            if (position==1)
+            {
+                if (historyFragment==null)
+                    historyFragment=new HistoryFragment();
+                return historyFragment;
+            }
             return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Current List";
                 case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+                    return "History";
             }
             return null;
         }

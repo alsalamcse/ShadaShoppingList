3c848a9828c8b280967add09a6ff7d8403e56df9
alsalamcc <alsalamcc17>
2017-10-23 09:45:20 +0300

    jkuij

--- app/src/main/java/com/hsarme/shada/shadashoppinglist/SignUp.java
+++ app/src/main/java/com/hsarme/shada/shadashoppinglist/SignUp.java
@@ -16,8 +16,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SignUp extends AppCompatActivity
-{
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
     private EditText etName, etEmail2, etPassword2, etRePassword, etPhone;
     private Button btnSave;
     private FirebaseAuth auth;
@@ -28,56 +27,44 @@ public class SignUp extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        etName=(EditText)findViewById(R.id.etName);
-        etEmail2=(EditText)findViewById(R.id.etEmail2);
-        etPassword2=(EditText)findViewById(R.id.etPassword);
-        etRePassword=(EditText)findViewById(R.id.etRePassword);
-        etPhone=(EditText)findViewById(R.id.etPhone);
-        btnSave=(Button)findViewById(R.id.btnSave);
-        auth= FirebaseAuth.getInstance();
-        firebaseUser= auth.getCurrentUser();
-
-
-        btnSave.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                dataHandler();
-            }
-        });
+        etName = (EditText) findViewById(R.id.etName);
+       etEmail2 = (EditText) findViewById(R.id.etEmail2);
+        etPassword2 = (EditText) findViewById(R.id.etPassword2);
+        etRePassword = (EditText) findViewById(R.id.etRePassword);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
     }
 
 
-    private void dataHandler()
-    {
-        String stEmail=etEmail2.getText().toString();
-        String stName=etName.getText().toString();
-        String stPassw=etPassword2.getText().toString();
-        String stRePassw=etRePassword.getText().toString();
-        String stPhone=etPhone.getText().toString();
-        boolean isOk=true;//to check id all feilds are filled correct!!!!
-        if (stEmail.length()==0||stEmail.indexOf('@')<1){
+    private void dataHandler() {
+        String email = etEmail2.getText().toString();
+        String stName = etName.getText().toString();
+        String passw = etPassword2.getText().toString();
+        String stRePassw = etRePassword.getText().toString();
+        String stPhone = etPhone.getText().toString();
+        boolean isOk = true;//to check id all feilds are filled correct!!!!
+        if (email.length() == 0 || email.indexOf('@') < 1) {
             etEmail2.setError("Wrong Email");
-            isOk=false;
+            isOk = false;
         }
-        if (stPassw.length()<8||stPassw.equals(stRePassw)==false){
-            etPassword2.setError("Bad Password");
-            isOk=false;
+        if (isOk) {
+            creatAcount(email, passw);
         }
-        creatAcount(stEmail,stPassw);
     }
 
 
-
-    private void creatAcount(String email,String passw){
+    private void creatAcount(String email, String passw) {
         auth.createUserWithEmailAndPassword(email, passw).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(SignUp.this,"Authentication Successful.",Toast.LENGTH_SHORT).show();
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignUp.this, "Authentication Successful.", Toast.LENGTH_SHORT).show();
                     finish();
-                }
-                else {
-                    Toast.makeText(SignUp.this,"Authentication Failed."+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SignUp.this, "Authentication Failed." + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     task.getException().printStackTrace();
                 }
             }
@@ -85,4 +72,10 @@ public class SignUp extends AppCompatActivity
         FirebaseAuth.getInstance().signOut();
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view==btnSave){
+            dataHandler();
+        }
+    }
 }

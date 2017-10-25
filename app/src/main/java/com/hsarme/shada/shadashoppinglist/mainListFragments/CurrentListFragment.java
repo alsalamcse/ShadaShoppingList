bbc8128067887577a45f541142ef939bc030db63
alsalamcc <alsalamcc17>
2017-10-25 13:23:04 +0300

    jkuij

--- app/src/main/java/com/hsarme/shada/shadashoppinglist/mainListFragments/CurrentListFragment.java
+++ app/src/main/java/com/hsarme/shada/shadashoppinglist/mainListFragments/CurrentListFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.hsarme.shada.shadashoppinglist.R;
 
@@ -13,6 +17,9 @@ import com.hsarme.shada.shadashoppinglist.R;
  * A simple {@link Fragment} subclass.
  */
 public class CurrentListFragment extends Fragment {
+    private ImageButton imbSave;
+    private TextView tvTotalValue, tvTotal, tvCount, tvCountValue;
+    private ListView lstTvCurrent;
 
 
     public CurrentListFragment() {
@@ -24,7 +31,19 @@ public class CurrentListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current_list, container, false);
+        View view= inflater.inflate(R.layout.fragment_current_list, container, false);
+        tvTotal=(TextView)view.findViewById(R.id.tvTotal);
+        tvTotalValue=(TextView)view.findViewById(R.id.tvTotalValue);
+        tvCount=(TextView)view.findViewById(R.id.tvCount);
+        tvCountValue=(TextView)view.findViewById(R.id.tvCountValue);
+        imbSave=(ImageButton)view.findViewById(R.id.imBSave);
+        lstTvCurrent=(ListView)view.findViewById(R.id.lstTvCurrent);
+
+        String[] ar={"Noor","Rimaa","Teya","Muhammed","Shada"};
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this, )
+
+
+        return view;
     }
 
 }
--- app/src/main/java/com/hsarme/shada/shadashoppinglist/mainListFragments/HistoryFragment.java
+++ app/src/main/java/com/hsarme/shada/shadashoppinglist/mainListFragments/HistoryFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.hsarme.shada.shadashoppinglist.R;
 
@@ -13,6 +14,8 @@ import com.hsarme.shada.shadashoppinglist.R;
  * A simple {@link Fragment} subclass.
  */
 public class HistoryFragment extends Fragment {
+
+    private ListView lstHistory;
 
 
     public HistoryFragment() {
@@ -24,7 +27,9 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View view= inflater.inflate(R.layout.fragment_history, container, false);
+        lstHistory=(ListView) view.findViewById(R.id.lstHistory);
+        return view;
     }
 
 }

c478f05e71da3098f672ddbf43cd60a40a5b8262
user <shada.hsarme@gmail.com>
2017-12-14 12:24:57 +0200

    111

--- app/src/main/java/com/hsarme/shada/shadashoppinglist/Data/ProductAdapter.java
+++ app/src/main/java/com/hsarme/shada/shadashoppinglist/Data/ProductAdapter.java
@@ -1,8 +1,41 @@
 package com.hsarme.shada.shadashoppinglist.Data;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by user on 14/12/2017.
  */
 
-public class ProductAdapter {
+public class ProductAdapter extends ArrayAdapter<Product>
+{
+    /**
+     *
+     * @param context
+     * @param resource the item xml file ex: R.layout.product_item
+     */
+    public ProductAdapter(@NonNull Context context, @LayoutRes int resource) {
+        super(context, resource);
+    }
+
+    /**
+     * todo
+     * @param position the index of one data item from the data source . statrting from zero
+     * @param convertView
+     * todo הממשק שיכטל להציג אוסף נתונים לדוגמה LISTVIEW
+     * @param parent listView
+     * @return
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view= LayoutInflater.from(getContext()).inflate(R.layout.product_item, parent, false);
+        return view;
+    }
 }
